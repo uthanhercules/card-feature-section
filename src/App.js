14eb38ae2636dc64078cfb520430966ac475b8d8
Uthan H <uthan.hercules12@gmail.com>
2021-11-15 03:59:28 -0300

    :lipstick: Style of the cards and page done

--- src/App.js
+++ src/App.js
@@ -9,12 +9,14 @@ import teamBuilderIcon from './assets/icon-team-builder.svg';
 
 const App = function MainApp() {
   return (
-    <>
+    <section className="cards">
       <Card color="cyan" name="Supervisor" icon={supervisorIcon} description="Monitors activity to identify projects roadblocks" />
-      <Card color="red" name="Team Builder" icon={teamBuilderIcon} description="Monitors activity to identify projects roadblocks" />
-      <Card color="orange" name="Karma" icon={karmaIcon} description="Monitors activity to identify projects roadblocks" />
+      <div className="center">
+        <Card color="red" name="Team Builder" icon={teamBuilderIcon} description="Monitors activity to identify projects roadblocks" />
+        <Card color="orange" name="Karma" icon={karmaIcon} description="Monitors activity to identify projects roadblocks" />
+      </div>
       <Card color="blue" name="Calculator" icon={calculatorIcon} description="Monitors activity to identify projects roadblocks" />
-    </>
+    </section>
   );
 };
 
